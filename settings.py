fdd1c91cd9648506029a42c1b389f8c483cb08ab
MikeMitusov <mitsivimail@gmail.com>
2024-02-13 12:22:31 +0200

    Structure

---
+++ settings.py
@@ -0,0 +1,34 @@
+# All servers are stored in https://github.com/iv-org/documentation/blob/master/docs/instances.md
+servers = [
+    "vid.puffyan.us",
+    "yt.artemislena.eu",
+    "invidious.flokinet.to",
+    "invidious.projectsegfau.lt",
+    "invidious.slipfox.xyz",
+    "invidious.privacydev.net",
+    "iv.melmac.space",
+    "iv.ggtyler.dev",
+    "invidious.lunar.icu",
+    "inv.nadeko.net",
+    "inv.tux.pizza",
+    "invidious.protokolla.fi",
+    "iv.nboeck.de",
+    "invidious.private.coffee",
+    "yt.drgnz.club",
+    "iv.datura.network",
+    "invidious.fdn.fr",
+    "invidious.perennialte.ch",
+    "yt.cdaut.de",
+    "invidious.drgns.space",
+    "inv.us.projectsegfau.lt",
+    "invidious.einfachzocken.eu",
+    "invidious.nerdvpn.de",
+    "inv.n8pjl.ca",
+    "youtube.owacon.moe",
+    "invidious.jing.rocks",
+]
+
+# Current server
+current_server = servers[
+    servers.index("invidious.private.coffee")
+]  # Choose server which is faster for you
